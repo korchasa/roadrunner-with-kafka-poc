638cbe3160a076affa6974e04546b6d0f0906044
korchasa <korchasa@syneforge.com>
2019-04-26 15:35:37 +0300

    fix: action name

--- php/src/Controller/DefaultController.php
+++ php/src/Controller/DefaultController.php
@@ -10,7 +10,7 @@ class DefaultController
         return new JsonResponse(["ok" => true]);
     }
 
-    public function lucky()
+    public function number()
     {
         $relay = new \Spiral\Goridge\SocketRelay("127.0.0.1", 6001);
         $rpc = new \Spiral\Goridge\RPC($relay);
